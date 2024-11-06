02011ff231930fc89c8a1a69172fda7f725be3e5
arbazdiwan <arbazdiwan@gmail.com>
2024-11-06 09:13:41 +0530

    chore: fix deployment issues

--- src/config/gcp.config.js
+++ src/config/gcp.config.js
@@ -9,9 +9,9 @@ const gcp = () => {
     admin.initializeApp({
       credential: admin.credential.cert({
         type: "service_account",
-        project_id: process.env.GOOGLE_CLOUD_PROJECT_ID,
-        private_key: process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT_PRIVATE_KEY,
-        client_email: process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+        privateKey: process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT_PRIVATE_KEY,
+        clientEmail: process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT_CLIENT_EMAIL,
       }),
       storageBucket: process.env.GOOGLE_CLOUD_STORAGE_BUCKET,
       databaseURL: process.env.GOOGLE_CLOUD_FIREBASE_DATABASE_URL,
